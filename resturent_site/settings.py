a8369aefb5b6b3c239354546117bfa9397774e63
Aynain Meraj <aynanmeraj@gmail.com>
2026-06-30 11:08:12 +0530

    Ready for deployment

--- resturent_site/settings.py
+++ resturent_site/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i^wp_s+ys-_sg7@l_i^v1#2%kx#-!p%dt5g-niam0!l%+!_rqo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
